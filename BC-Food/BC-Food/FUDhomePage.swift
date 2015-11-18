f23838d05e828c7b394221a72958d9bdb7157d12
SeungWan <SeungWan@admins-macbook-air.local>
2015-11-18 00:37:44 -0500

    Configured coding for passing dining hall name from dining hall home page to menu screen. The next part is using the transferred string (dining hall name) as a key to access select data from specific arrays/dictionaries. I created a dummy array for each dining hall containing all of the possible food items. They are split up in the following way: segmented view > section > food item, where segmented view = breakfast, lunch, dinner, or late night and section = Food Types 1-4.
    I will try using dictionaries next time to deal with the complexity of organizing the food items.
    
    Besides styling the screens, I think I'm at the end of my work for this branch!

--- BC-Food/BC-Food/FUDhomePage.swift
+++ BC-Food/BC-Food/FUDhomePage.swift
@@ -22,50 +22,82 @@ class FUDhomePage: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    var DiningName = "test"
+    
+    @IBOutlet weak var MacButton: UIButton!
     @IBAction func MacSegue(sender: AnyObject) {
+        self.DiningName = "Mac"
         performSegueWithIdentifier("general", sender: nil)
         print("Mac general segue works!!")
         
     }
     
+    @IBOutlet weak var EagleButton: UIButton!
     @IBAction func EagleSegue(sender: AnyObject) {
+        self.DiningName = "Eagle's Nest"
         performSegueWithIdentifier("general", sender: nil)
         print("Eagle's general segue works!!")
     
     }
 
     
+    @IBOutlet weak var RatButton: UIButton!
     @IBAction func RatSegue(sender: AnyObject) {
+        self.DiningName = "The Rat"
         performSegueWithIdentifier("general", sender: nil)
         print("Rat's general segue works!!")
     }
    
     
+    @IBOutlet weak var HillsideButton: UIButton!
     @IBAction func HillsideSegue(sender: AnyObject) {
+        self.DiningName = "Hillside"
         performSegueWithIdentifier("general", sender: nil)
         print("Hillside's general segue works!!")
     }
                     
+    @IBOutlet weak var LowerButton: UIButton!
     @IBAction func LowerSegue(sender: AnyObject) {
+        self.DiningName = "Lower"
         performSegueWithIdentifier("general", sender: nil)
         print("Lower's general segue works!!")
     }
     
     
+    @IBOutlet weak var AddieButton: UIButton!
     @IBAction func AddieSegue(sender: AnyObject) {
+        self.DiningName = "Addie's"
         performSegueWithIdentifier("general", sender: nil)
         print("Addie's general segue works!!")
     }
     
+    @IBOutlet weak var StuartButton: UIButton!
     @IBAction func StuartSegue(sender: AnyObject) {
+        self.DiningName = "Stuart"
         performSegueWithIdentifier("general", sender: nil)
         print("Stuart's general segue works!!")
     }
     
     
+    @IBOutlet weak var BrightonButton: UIButton!
     @IBAction func BrightonSegue(sender: AnyObject) {
+        self.DiningName = "Brighton"
         performSegueWithIdentifier("general", sender: nil)
         print("Brighton's general segue works!!")
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        if segue.identifier == "general" {
+            print("general")
+            if let nextVC: FUDdiningHallHome = segue.destinationViewController as? FUDdiningHallHome{
+                print("accessing third screen from second")
+                nextVC.DiningPlace = DiningName
+                nextVC.TitleOfMenu = DiningName 
+                
+            }
+            
+        }
     }
 
     /*
